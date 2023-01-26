e6f54158d4ac2cf94140d692c184e1b4d9826b85
EternalLabrynth <eternallabrynth@gmail.com>
2023-01-26 10:20:41 -0800

    Test

--- Lab1/src/lab1.py
+++ Lab1/src/lab1.py
@@ -6,11 +6,11 @@
 def readEncoders():
     pinb6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
     pinb7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
-    tim2 = pyb.Timer(2, freq = 20000)
-    ch1 = tim2.channel(2, pyb.Timer.ENC_AB, pin=pinb6)
-    ch2 = tim2.channel(2, pyb.Timer.ENC_AB, pin=pinb7)
+    tim4 = pyb.Timer(4, prescaler = 0, period = 0xFFFF)
+    ch1 = tim4.channel(4, pyb.Timer.ENC_AB, pin=pinb6)
+    ch2 = tim4.channel(4, pyb.Timer.ENC_AB, pin=pinb7)
 
-    
+
 
 
 
