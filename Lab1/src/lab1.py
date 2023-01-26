93750a24896b82c6e3fdbb02219ba106425074e3
EternalLabrynth <eternallabrynth@gmail.com>
2023-01-26 10:13:47 -0800

    readEncoders() updated

--- Lab1/src/lab1.py
+++ Lab1/src/lab1.py
@@ -4,9 +4,13 @@
 
 # encoder reading code
 def readEncoders():
-    pass
+    pinb6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+    pinb7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
+    tim2 = pyb.Timer(2, freq = 20000)
+    ch1 = tim2.channel(2, pyb.Timer.ENC_AB, pin=pinb6)
+    ch2 = tim2.channel(2, pyb.Timer.ENC_AB, pin=pinb7)
 
-
+    
 
 
 
