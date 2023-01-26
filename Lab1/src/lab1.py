12e5589bc4b5050a17028ca7b476427359ef88fa
EternalLabrynth <eternallabrynth@gmail.com>
2023-01-26 09:25:53 -0800

    Implemented base lab1 functions

--- Lab1/src/lab1.py
+++ Lab1/src/lab1.py
@@ -1,1 +1,23 @@
 # Lab 1 Python Code
+
+
+
+# encoder reading code
+def readEncoders():
+    pass
+
+
+
+
+
+# main
+def main():
+    readEncoders()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
